d15f8172817f6686bf006cecc79a8db01965d6b1
rfrech <rfrechette@cnm.edu>
2020-10-13 13:57:08 -0700

    spinners in progress crashing.

--- app/src/main/java/edu/cnm/deepdive/codebreaker/controller/GameFragment.java
+++ app/src/main/java/edu/cnm/deepdive/codebreaker/controller/GameFragment.java
@@ -11,6 +11,7 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Spinner;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
@@ -18,6 +19,7 @@ import androidx.fragment.app.FragmentActivity;
 import androidx.lifecycle.LifecycleOwner;
 import androidx.lifecycle.ViewModelProvider;
 import edu.cnm.deepdive.codebreaker.R;
+import edu.cnm.deepdive.codebreaker.adapter.CodeCharacterAdapter;
 import edu.cnm.deepdive.codebreaker.adapter.GuessAdapter;
 import edu.cnm.deepdive.codebreaker.databinding.FragmentGameBinding;
 import edu.cnm.deepdive.codebreaker.viewmodel.MainViewModel;
@@ -27,15 +29,16 @@ import java.util.Map;
 public class GameFragment extends Fragment  implements InputFilter {
 
   private static final String INVALID_CHAR_PATTERN = String.format("[^%s]", MainViewModel.POOL);
-  private int[] colorValues;
+
   private Map<Character, Integer> colorValueMap;
   private Map<Character, String> colorLabelMap;
-
+  private Character[] codeCharacters;
 
   private MainViewModel viewModel;
   private GuessAdapter adapter;
   private int codeLength;
   private FragmentGameBinding binding;
+  private Spinner[] spinners;
 
   @Override
   public void onCreate(Bundle savedInstanceState) {
@@ -48,15 +51,11 @@ public class GameFragment extends Fragment  implements InputFilter {
   public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
       @Nullable Bundle savedInstanceState) {
     binding = FragmentGameBinding.inflate(getLayoutInflater());
+    setupMaps();
     setupViews();
-    char[] colorCodes = getString(R.string.color_codes).toCharArray();
-    Resources resources = getContext().getResources();
-    int[] colorValues = resources.getIntArray(R.array.color_values);
-    String[] colorLabels = resources.getStringArray(R.array.color_labels);
-    colorValueMap = buildColorMap(colorCodes, colorValues);
-    colorLabelMap = buildLabelMap(colorCodes, colorLabels);
     return binding.getRoot();
   }
+
 
   @Override
   public void onViewCreated(@NonNull View view,
@@ -103,9 +102,32 @@ public class GameFragment extends Fragment  implements InputFilter {
     return modifiedSource;
   }
 
+  private void setupMaps() {
+    char[] colorCodes = getString(R.string.color_codes).toCharArray();
+    this.codeCharacters = getString(R.string.color_codes).chars()
+        .mapToObj((value) -> (char) value)
+        .toArray(Character[]::new);
+    Resources resources = getResources();
+    int[] colorValues = resources.getIntArray(R.array.color_values);
+    String[] colorLabels = resources.getStringArray(R.array.color_labels);
+    colorValueMap = buildColorMap(colorCodes, colorValues);
+    colorLabelMap = buildLabelMap(colorCodes, colorLabels);
+  }
+
   private void setupViews() {
-    binding.guess.setFilters(new InputFilter[]{this});
     binding.submit.setOnClickListener((view) -> recordGuess());
+    int maxCodeLength = getResources().getInteger(R.integer.code_length_pref_max);
+    spinners = new Spinner[maxCodeLength];
+    LayoutInflater inflater = LayoutInflater.from(getContext());
+    for (int i = 0; i < maxCodeLength; i++) {
+      Spinner spinner =
+          (Spinner) inflater.inflate(R.layout.swatch_spinner, binding.guessControls, false);
+      CodeCharacterAdapter adapter = new CodeCharacterAdapter(
+          getContext(), colorValueMap, colorLabelMap, codeCharacters, spinner);
+      spinner.setAdapter(adapter);
+      spinners[i] = spinner;
+      binding.guessControls.addView(spinner, i);
+    }
   }
 
   private void setupViewModel() {
@@ -114,20 +136,21 @@ public class GameFragment extends Fragment  implements InputFilter {
     adapter = new GuessAdapter(activity, colorValueMap, colorLabelMap);
     viewModel = new ViewModelProvider(activity).get(MainViewModel.class);
     LifecycleOwner lifecycleOwner = getViewLifecycleOwner();
-    viewModel.getGame().observe(lifecycleOwner, (game) -> {
-      adapter.clear();
-      adapter.addAll(game.getGuesses());
-      binding.guessList.setAdapter(adapter);
-      binding.guessList.setSelection(adapter.getCount() - 1);
-      codeLength = game.getLength();
-      binding.guess.setText("");
-    });
+    viewModel.getGame().observe(lifecycleOwner, this::updateGameDisplay);
     viewModel.getSolved().observe(lifecycleOwner, solved ->
         binding.guessControls.setVisibility(solved ? View.INVISIBLE : View.VISIBLE));
   }
 
+  private void updateGameDisplay(edu.cnm.deepdive.codebreaker.model.Game game) {
+    adapter.clear();
+    adapter.addAll(game.getGuesses());
+    binding.guessList.setAdapter(adapter);
+    binding.guessList.setSelection(adapter.getCount() - 1);
+    codeLength = game.getLength();
+  }
+
   private void recordGuess() {
-    viewModel.guess(binding.guess.getText().toString().trim().toUpperCase());
+//    viewModel.guess(binding.guess.getText().toString().trim().toUpperCase());
   }
 
   private void startGame() {
